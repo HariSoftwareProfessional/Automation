40dfdb9bebf96d33a0f7f37a5862d0c17413ebb2
Hari Software <36462649+softwaregeekcode@users.noreply.github.com>
2019-04-30 00:03:21 -0300

    version 1.0
    commit

---
+++ dataConfigs/src/main/java/dataProviders/CsvFileReader.java
@@ -0,0 +1,31 @@
+package dataProviders;
+
+import java.io.FileReader;
+
+import com.opencsv.CSVReader;
+
+import managers.FileReaderManager;
+
+public class CsvFileReader {
+
+	public final static String csvFilePath = FileReaderManager.getInstance().getPropertyFileDataReader()
+			.getTestDataResourcePath() + "csvData/customer.csv";
+
+	public static void readCsvDataLinebyLine() {
+		try {
+			FileReader fileReader = new FileReader(csvFilePath);
+			CSVReader csvReader = new CSVReader(fileReader);
+			String[] nextRecord;
+			System.out.println("----Read Data from CSV file-------");
+			// we are going to read data line by line
+			while ((nextRecord = csvReader.readNext()) != null) {
+				for (String cell : nextRecord) {
+					System.out.println(cell);
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
